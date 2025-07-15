072b2b9df7b457dbc30243a567368c92694ab8af
Mikhael Fourie <mikhael.fourie@gmail.com>
2025-07-15 09:24:12 +0200

    Changed pool connection

--- server/db.js
+++ server/db.js
@@ -1,26 +1,9 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
 const pool = new Pool({
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-
-  // connectionString: process.env.SUPABASE_DB_URL,
-  // ssl: { rejectUnauthorized: false },
-
-  connectionString: process.env.SUPABASE_DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  family: 4,
 });
 
 module.exports = pool;
